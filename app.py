5f17c9a797ee8de6895cb7959ec7843df123e1ee
Heylim Kim <hlim9022@gmail.com>
2022-07-11 18:59:32 +0900

    css 파일 링크 업데이트

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 app = Flask(__name__)
 
 from pymongo import MongoClient
@@ -17,8 +17,9 @@ def register():
 
 @app.route('/main')
 def main():
-    myname = 'kelly'
+    myname = 'C반3조'
     return render_template('recommend.html', name=myname)
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
