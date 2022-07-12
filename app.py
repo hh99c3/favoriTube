29f42afd4dd40cc8a1cb5c884b0be57bc2eb5198
oneqrhee <blaackpurin@gmail.com>
2022-07-12 16:03:10 +0900

    로그인/회원가입

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import hashlib
+from datetime import timedelta
+
+import jwt
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 app = Flask(__name__)
 
@@ -5,18 +9,71 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.ydgg3.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.hanghae
 
+SECRET_KEY = 'SPARTA'
+
 @app.route('/')
+def home():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+
+        return render_template('recommend.html')
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+@app.route('/login')
 def login():
-    return render_template('login.html')
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        from datetime import datetime
+        payload = {
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 @app.route('/membership')
 def register():
     return render_template('register.html')
 
-@app.route('/main')
-def main():
-    myname = 'C반3조'
-    return render_template('recommend.html', name=myname)
+@app.route('/membership/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    category_receive = request.form['category_give']
+
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,
+        "password": password_hash,
+        "category": category_receive
+     }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+@app.route('/membership/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
