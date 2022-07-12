38b1a0b24d1587d6fc254020909f5d8a10679528
ghwo68 <ghwo68@naver.com>
2022-07-12 18:30:45 +0900

    Revert "no message"
    This reverts commit cda1c42f87ac9530a1de09ea64912ecb05631ba1.

--- app.py
+++ app.py
@@ -75,48 +75,5 @@ def check_dup():
     exists = bool(db.users.find_one({"username": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
 
-@app.route('/main')
-def main():
-    myname = 'C반3조'
-    return render_template('main.html', name=myname)
-
-@app.route('/recommend/<keyword>')
-def recommend(keyword):
-    myname = 'C반3조'
-    users = list(db.users.find({}, {'_id': False}))
-    return render_template('recommend.html', users= users , name = myname , word = keyword)
-
-@app.route('/subscribe')
-def subscribe():
-    myname = 'C반3조'
-    return render_template('subscribe.html', name = myname)
-
-@app.route("/subscribe", methods=["POST"])
-def subscribe_post():
-    title_receive = request.form['title_give']
-    url_receive = request.form['url_give']
-    cate_receive = request.form['cate_give']
-    comment_receive = request.form['comment_give']
-
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
-
-    soup = BeautifulSoup(data.text, 'html.parser')
-
-    image = soup.select_one('meta[property="og:image"]')['content']
-
-
-    doc = {
-        'url': url_receive,
-        'title': title_receive,
-        'image': image,
-        'cate': cate_receive,
-        'comment': comment_receive
-    }
-    db.users.insert_one(doc)
-
-    return jsonify({'msg': '추가 완료!'})
-
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
