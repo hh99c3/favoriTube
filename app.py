17c300d5fe1e5e1f524f84b45c4821e756752ba5
oneqrhee <blaackpurin@gmail.com>
2022-07-12 09:22:40 +0900

    db폴더명 변경

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
 
 from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.ydgg3.mongodb.net/Cluster0?retryWrites=true&w=majority')
-db = client.dbsparta
-
+db = client.hanghae
 
 @app.route('/')
 def login():
@@ -14,12 +13,10 @@ def login():
 def register():
     return render_template('register.html')
 
-
 @app.route('/main')
 def main():
     myname = 'C반3조'
     return render_template('recommend.html', name=myname)
-
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
