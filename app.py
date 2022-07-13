bf1b830fa8a7b2c3c7703fb83607726fc887239c
oneqrhee <blaackpurin@gmail.com>
2022-07-13 15:43:42 +0900

    유저별 마이리스트 추가

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ from pymongo import MongoClient
 
 import certifi
 
-client = MongoClient('mongodb://test:sparta@ac-arc7jbv-shard-00-00.lfxuxob.mongodb.net:27017,ac-arc7jbv-shard-00-01.lfxuxob.mongodb.net:27017,ac-arc7jbv-shard-00-02.lfxuxob.mongodb.net:27017/Cluster0?ssl=true&replicaSet=atlas-3juyq2-shard-0&authSource=admin&retryWrites=true&w=majority', tlsCAFile=certifi.where())
-db = client.dbsparta
+client = MongoClient('mongodb+srv://test:sparta@cluster0.ydgg3.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.hanghaec
 
 
 SECRET_KEY = 'SPARTA'
@@ -161,6 +161,9 @@ def subscribe():
 def subscribe_post():
     token_receive = request.cookies.get('mytoken')
     try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({'username': payload['id']})
+        username = user_info['username']
         title_receive = request.form['title_give']
         url_receive = request.form['url_give']
         cate_receive = request.form['cate_give']
@@ -175,6 +178,7 @@ def subscribe_post():
         image = soup.select_one('meta[property="og:image"]')['content']
 
         doc = {
+            'username' : username,
             'url': url_receive,
             'title': title_receive,
             'image': image,
